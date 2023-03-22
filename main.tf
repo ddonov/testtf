467d476a4753b41d579914192ca2f4e1238dea85
Deyan Donovski <ddonovski@vmware.com>
2023-03-22 10:48:33 +0200

    2

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
+}
+
+resource "random_string" "random" {
+  length           = 8
+  special          = false
+  override_special = "/@£$"
+}
+
+output "random-uuid" {
+  value = uuid()
+}
+
+output "random-string" {
+  value 
